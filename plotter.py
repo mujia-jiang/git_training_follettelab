cb8368a5de5eb2a1a802b71270ec1a2baa731f0f
Nat Kerman <nathaniel.kerman@gmail.com>
2026-06-25 14:15:31 -0400

    add plots path

--- plotter.py
+++ plotter.py
@@ -1,2 +1,16 @@
+# %%
+from pathlib import Path
+
 import numpy as np
 import matplotlib.pyplot as plt
+# %%
+x = np.loadtxt('dir1/x.txt')
+y = np.loadtxt('dir2/y.txt')
+plt.plot(x, y, "o-")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.title("What a nice plot!")
+
+plot_dir = Path("plots")
+plot_dir.mkdir(exist_ok=True)
+plt.savefig(plot_dir / "my_plot.png")
